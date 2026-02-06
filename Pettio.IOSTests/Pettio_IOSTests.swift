a808829f7f98573081eadb9443f3d6c7d1712b4d
Roman Vorozhbyt <romanvorozhbyt199898@gmail.com>
2026-02-06 21:41:20 +0200

    Initial Commit

---
+++ Pettio.IOSTests/Pettio_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pettio_IOSTests.swift
+//  Pettio.IOSTests
+//
+//  Created by Катерина Каверина on 06.02.2026.
+//
+
+import Testing
+@testable import Pettio_IOS
+
+struct Pettio_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
